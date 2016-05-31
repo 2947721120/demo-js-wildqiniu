127c96cea90842ccb119457176d6ab4bc82ed61f
2947721120 <2947721120@users.noreply.github.com>
2016-05-31 13:49:03 +0800

    Update conf.js

--- demo/js/conf.js
+++ demo/js/conf.js
@@ -1,8 +1,8 @@
 var conf =  {
             'wilddog' : {
-    	     "baseurl":"https://myoffice1.wilddogio.com/messages" //野狗url
+    	     "baseurl":"https://c2cbc.wilddogio.com//messages" //野狗url
     },
     'qiniu' :{
-      	"imageBaseUrl":"http://7xklh8.com1.z0.glb.clouddn.com/"  //七牛存储图片的baseurl
+      	"imageBaseUrl":"http://o8065rd96.bkt.clouddn.com/"  //七牛存储图片的baseurl
     }
 }
