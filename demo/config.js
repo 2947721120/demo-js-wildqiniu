6e90bea674277d13751c4397b9fecf95455cbf43
2947721120 <2947721120@users.noreply.github.com>
2016-05-31 15:54:21 +0800

    Update config.js

--- demo/config.js
+++ demo/config.js
@@ -2,7 +2,7 @@ module.exports = {
      	 'ACCESS_KEY': 'kQTq0TBLizzZopNeWGEHgMxQfORCDp0iFtyPG_R7',
                 'SECRET_KEY': 'R0gfQt2Yp5aGLZ4GJ1HKWQlMSemGWk4zJsZibyhw',
                 'Bucket_Name': 'cdnmall',
-                'Port': 8080,
+                'Port': 44444,
                 'Uptoken_Url':'/uptoken',
                 'Domain': 'http://qinniu-plupload.com/'
 };
