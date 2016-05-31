a328d95531c4e1586850d8d651d0bb6d20133674
2947721120 <2947721120@users.noreply.github.com>
2016-05-31 15:29:23 +0800

    Update config.js

--- demo/config.js
+++ demo/config.js
@@ -2,7 +2,7 @@ module.exports = {
      	 'ACCESS_KEY': 'kQTq0TBLizzZopNeWGEHgMxQfORCDp0iFtyPG_R7',
                 'SECRET_KEY': 'R0gfQt2Yp5aGLZ4GJ1HKWQlMSemGWk4zJsZibyhw',
                 'Bucket_Name': 'cdnmall',
-                'Port': 18080,
+                'Port': 8080,
                 'Uptoken_Url': '/uptoken',
                 'Domain': 'http://o8065rd96.bkt.clouddn.com/'
 };
